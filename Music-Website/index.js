6b12cd13e5ca37889d7e19166332c7d312d17a0f
Vinh Quang <hell967452@gmail.com>
2021-03-12 23:04:50 +0700

    Quang Update For Long

--- Music-Website/index.js
+++ Music-Website/index.js
@@ -1,17 +1,3 @@
-// Click Search Bar
-var searchbar = document.querySelector("#search");
-var songtext = document.querySelector("#txtbaihat");
-var singertext = document.querySelector("#txtcasi");
-function slide(){
-    
-    if(searchbar.classList.contains("hide")){
-        searchbar.classList.remove("hide");
-        songtext.value =('');
-         singertext.value =('');
-    }else{
-         searchbar.classList.add("hide");
-    }
-}
 $(document).ready(function() {
     $(".Charlie-Yue-info").click(function() {
         $(".content").load("info.html");
@@ -19,11 +5,22 @@ $(document).ready(function() {
     $(".upmusic").click(function() {
         $(".content").load("uploadfile-v1.html");
     });
-    $(".buttoneupdate").click(function() {
-
-        alert("sadasds")
-    });
 });
+// Click Search Bar
+var searchbar = document.querySelector("#search");
+var songtext = document.querySelector("#txtbaihat");
+var singertext = document.querySelector("#txtcasi");
+
+function slide() {
+    if (searchbar.classList.contains("hide")) {
+        searchbar.classList.remove("hide");
+        songtext.value = ('');
+        singertext.value = ('');
+    } else {
+        searchbar.classList.add("hide");
+    }
+}
+
 const previous = document.querySelector('#previous');
 const play = document.querySelector('#play');
 const next = document.querySelector('#next');
@@ -82,7 +79,7 @@ var list_thieu_nhi = [];
 var list_us_uk = [];
 var list_bolero = [];
 var list_blue_jazz = [];
-var list_search=[];
+var list_search = [];
 
 function loadXML() {
     try {
@@ -358,18 +355,17 @@ function justPlay() {
 function loadListSong(prefixID, array) {
     const top_song = document.querySelector('#top_song');
     count = 0;
-    if(array.length>0){
+    if (array.length > 0) {
         for (let i = 0; i < array.length; i++) {
             count = count.toString().trim();
             var s = '<div class="song"><img class="song-img" src="' + array[i].image +
                 '"><div class="song-title"><span class="title">' + array[i].name + '</span><span>' + array[i].singer + '</span></div><a href="#" class="btn-song-play"  onclick="getSongToPlay(this.id)" id="' + addNewID(prefixID, i) + '"><i class="far fa-play-circle"></i></a></div>';
-    
+
             count = Number(count);
             top_song.insertAdjacentHTML('beforeend', s);
-    
+
         }
-    }
-    else{
+    } else {
         var s = '<div class="song"><img class="song-img" src="./images/502-error.png"/><div class="song-title"><span class="title">Không Tìm Thấy Kết Quả Tương Ứng!</span></div>';
         top_song.insertAdjacentHTML('beforeend', s);
     }
@@ -482,27 +478,28 @@ function range_slider() {
         }
     }
 }
+
 function Search() {
     songtxt = $('#txtbaihat').val();
     singertxt = $('#txtcasi').val();
-    list_search =[];
-    for(let i=0;i<songs.length;i++){
-        if(songs[i].name.toLowerCase().includes(songtxt.toLowerCase()) && songs[i].singer.toLowerCase().includes(singertxt.toLowerCase()) ){
+    list_search = [];
+    for (let i = 0; i < songs.length; i++) {
+        if (songs[i].name.toLowerCase().includes(songtxt.toLowerCase()) && songs[i].singer.toLowerCase().includes(singertxt.toLowerCase())) {
             list_search.push({
                 name: songs[i].name,
                 path: songs[i].path,
                 image: songs[i].image,
                 singer: songs[i].singer,
-    
+
             })
         }
     }
     console.log(list_search);
-    if(list_search.length >0){
+    if (list_search.length > 0) {
         removeElement();
         document.querySelector('#banner-top-song').innerHTML = "Kết Quả Tìm Kiếm";
         loadListSong("tt", list_search);
-    }else{
+    } else {
         removeElement();
         document.querySelector('#banner-top-song').innerHTML = "Kết Quả Tìm Kiếm";
         loadListSong("tt", list_search);
